5f3b7cdca57a1d30df9d3dc3a1df8097b5f3a46e
Py Colors <patweb44@gmail.com>
2019-04-25 21:19:12 +0100

    Handling Basic Requests : http.Handle

---
+++ src/example/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type myHandler struct {
+	greeting string
+}
+
+// Method take two argument ResponseWriter and a pointer http.Request
+func (mh myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("%v world", mh.greeting)))
+}
+
+func main() {
+	http.Handle("/", &myHandler{greeting: "Hello"})
+	http.ListenAndServe(":8000", nil)
+}
