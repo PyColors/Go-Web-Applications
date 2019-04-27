2e11945fa0062f6fb106781d30eb39c3709c8a08
PyColors <patweb44@gmail.com>
2019-04-27 13:51:36 +0100

    Demo: http.HandleFunc

--- src/example/main.go
+++ src/example/main.go
@@ -1,20 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-type myHandler struct {
-	greeting string
-}
-
-// Method takes two argument ResponseWriter and a pointer http.Request
-func (mh myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("%v world", mh.greeting)))
-}
+import "net/http"
 
 func main() {
-	http.Handle("/", &myHandler{greeting: "Hello"})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
 	http.ListenAndServe(":8000", nil)
 }
